12fc588838ad6d8bb0bd8456fca8a93a78b33954
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-07-13 16:05:17 +1000

    Create main.php

---
+++ src/main.php
@@ -0,0 +1,18 @@
+<?php
+namespace skyss0fly\PocketMal;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+
+public function __construct(private readonly LunarRanksPlugin $plugin)
+    {
+        $this->setPermission("pocketmal.command.rby");
+
+        $this->commandArg = new CommandArgs();
+        $this->commandArg->addParameter(0, "player", AvailableCommandsPacket::ARG_TYPE_TARGET);
+        $key = $this->commandArg->addParameter(0, "rank", AvailableCommandsPacket::ARG_FLAG_ENUM | AvailableCommandsPacket::ARG_TYPE_STRING);
+        $this->commandArg->setEnum(0, $key, "rank", $this->plugin->getRankList());
+
+        $command = $this->plugin->getMessages()["rank-command"];
+        parent::__construct($command["name"], $command["description"], $command["global-usage"], $command["aliases"]);
+    }
