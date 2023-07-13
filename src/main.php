2a8a160e6840ac79efa65a50e05f708f60133773
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-07-13 16:06:51 +1000

    Update main.php

--- src/main.php
+++ src/main.php
@@ -3,7 +3,10 @@ namespace skyss0fly\PocketMal;
 
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
+use pocketmine\plugin\PluginBase;
 
+class Main extends PluginBase {
+    
 public function __construct(private readonly LunarRanksPlugin $plugin)
     {
         $this->setPermission("pocketmal.command.rby");
@@ -16,3 +19,4 @@ public function __construct(private readonly LunarRanksPlugin $plugin)
         $command = $this->plugin->getMessages()["rank-command"];
         parent::__construct($command["name"], $command["description"], $command["global-usage"], $command["aliases"]);
     }
+} 
