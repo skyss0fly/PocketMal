96b526a25553ed354bdaea5109705d20c98c5934
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-07-13 16:09:37 +1000

    Update main.php

--- src/main.php
+++ src/main.php
@@ -7,16 +7,10 @@ use pocketmine\plugin\PluginBase;
 
 class Main extends PluginBase {
     
-public function __construct(private readonly LunarRanksPlugin $plugin)
-    {
-        $this->setPermission("pocketmal.command.rby");
+public function onEnable(): void {
+$this->savedefaultconfig();
+    
 
-        $this->commandArg = new CommandArgs();
-        $this->commandArg->addParameter(0, "player", AvailableCommandsPacket::ARG_TYPE_TARGET);
-        $key = $this->commandArg->addParameter(0, "rank", AvailableCommandsPacket::ARG_FLAG_ENUM | AvailableCommandsPacket::ARG_TYPE_STRING);
-        $this->commandArg->setEnum(0, $key, "rank", $this->plugin->getRankList());
+}
 
-        $command = $this->plugin->getMessages()["rank-command"];
-        parent::__construct($command["name"], $command["description"], $command["global-usage"], $command["aliases"]);
-    }
-} 
+}
